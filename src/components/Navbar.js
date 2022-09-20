23351e824915609e7a3ce86f238fa31f8848bd9f
EVZODA <enzogiacoia@hotmail.com>
2022-09-20 15:28:49 -0300

    carrito desplegable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cart from './Cart'
 import lapetite from '../img/lapetite.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -25,25 +26,41 @@ const Navbar = () => {
             <a href="../App.js" className='md:mr-3'>About</a> 
             <a href="../App.js" className='md:mr-3'>Producto</a> 
             <a href="../App.js" className='md:mr-3'>Contacto</a> 
-            <a href="../App.js" className='md:mr-3'><FontAwesomeIcon icon="fa-solid fa-cart-shopping" /></a> 
+            <a href="#" className='dropdown-cart flex justify-center relative md:mr-3 md:justify-end '><FontAwesomeIcon onClick={dropdown} icon="fa-solid fa-cart-shopping" />
+            <div className='dropdown-menu text-center absolute translate-y-[26px] w-[100px] md:w-[107px] pointer-events-none opacity-0 bg-white text-black p-1 rounded-[5px] shadow'>
+                <div className='dropdown-item p-1 rounded-[3px] hover:bg-slate-200'>torta</div>
+                <div className='dropdown-item p-1 rounded-[3px] hover:bg-slate-200'>torta</div>
+                <div className='dropdown-item p-1 rounded-[3px] hover:bg-slate-200'>torta</div>
+                <div className='dropdown-item p-1 rounded-[3px] hover:bg-slate-200'>torta</div>
+            </div>
+            </a> 
         </div>
     </nav>
   )
 }
-
+let expandeddrop = false
 let expanded = false,
     $navbar,
     $navLink,
+    $dropdownh,
+    $dropdownitem,
     navbarH,
-    navLinkH;
+    navLinkH,
+    dropdownh,
+    dropdownitem;
+
 
 
 window.addEventListener('load', () => {
 
     $navbar = document.querySelector('.navbar');
     $navLink = document.querySelector('.nav-link');
+    $dropdownh = document.querySelector('.dropdown-menu')
+    $dropdownitem = document.querySelector('.dropdown-item')
     navbarH = $navbar.clientHeight;
     navLinkH = $navLink.clientHeight;
+    dropdownh = $dropdownh.clientHeight;
+    dropdownitem = $dropdownitem.clientHeight;
 
     if(window.innerWidth < 768) {
         $navbar.style.height = navbarH - navLinkH + 'px'
@@ -64,9 +81,27 @@ const expandNav = () => {
         expanded = true
     } else {
         $navbar.style.height = navbarH - navLinkH + 'px'
+        if (expandeddrop) {
+            dropdown()
+        }
         expanded = false
     }
     
 }
 
+const dropdown = () => {
+    if (!expandeddrop) {
+        $dropdownh.style.height = dropdownh + 'px'
+        $navbar.style.overflow = "visible"
+        $dropdownh.classList.remove("opacity-0")
+        $dropdownh.classList.remove("pointer-events-none")
+        expandeddrop = true
+    } else {
+        $dropdownh.style.height = dropdownh - dropdownitem + 'px'
+        $dropdownh.classList.add("opacity-0")
+        $dropdownh.classList.add("pointer-events-none")
+        expandeddrop = false
+    }
+    
+}
 export default Navbar
