340ce2b0f496931e47fb9595a40a84dd1b07ac0f
Xen0mrefne <x3n0g3n1c@gmail.com>
2022-09-29 11:29:15 -0300

    formik

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import  Navbar  from './components/Navbar'
 import ShoppingContextProvider from './components/ShoppingContextProvider'
-import ProductSection from './components/ProductSection'
 import './navbar.css'
 
 
@@ -8,7 +7,9 @@ function App() {
 	return <>
 		<ShoppingContextProvider>
 			<Navbar/>
-			<ProductSection/>
+
+			{/* Componente de tarjetas para carrito aqui */}
+
 		</ShoppingContextProvider>
 	</>
 }
