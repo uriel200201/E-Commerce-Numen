15c41144416f40894263797d37c8c4a12ac87154
Xen0mrefne <x3n0g3n1c@gmail.com>
2022-10-04 13:18:29 -0300

    item-counter-finished

--- src/components/navbar/ShoppingCart.js
+++ src/components/navbar/ShoppingCart.js
@@ -11,7 +11,7 @@ const ShoppingCart = () => {
   
   return (
     <>
-        <div className='dropdown-menu flex flex-col w-[285px] items-center absolute translate-y-[26px] pointer-events-none opacity-0 bg-white text-black p-1 rounded-[5px] shadow'>
+        <div className='dropdown-menu flex flex-col w-[285px] items-center absolute translate-y-[40px] pointer-events-none opacity-0 bg-white text-black p-1 rounded-[5px] shadow'>
             <div className="dropdown-items overflow-y-scroll">
               {state.cart.map((item, index) => <CartItem key={index} info={item} deleteFromCart={deleteFromCart}/>)}
             </div>
