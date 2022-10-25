3e710ada2d06394d08e17def29e31a1d714a13a2
julianatelesca@gmail <julianatelesca@gmail.com>
2022-10-24 21:57:53 -0300

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
 import React from 'react';
 import Footer from './components/Footer';
+import Map from './components/Map';
+
+
 function App() {
-return (
-	<Footer />
-);
+	return (
+		<Footer />,
+		< Map/>
+		
+	);		
 }
 
-export default App;
+
+export default App
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import logo from '../../src/img/logo.png';
 
-const texto = "El toque de dulzura que vos necesitas"
-
+const texto = "EL TOQUE DE DULZURA QUE VOS NECESITAS"
 const Footer = () => {
   return (
     
-<footer class="p-4 bg-slate-400 shadow md:px-6 md:py-8 dark:bg-gray-900">
+<footer class="p-4 bg-gray-200 shadow md:justify-around dark:bg-gray-200">
     <div class="sm:flex sm:items-center sm:justify-between">
         <a href="" class="flex items-center mb-4 sm:mb-0">
-            <img src={logo} class="bg-gray-500 w-20 h-20 rounded-full" alt="Logo"/>
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{texto}</span>
+            <img src={logo} class="bg-gray-400 w-20 h-20 rounded-full" alt="Logo"/>
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{texto}</span>
         </a>
-        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+        <ul class="flex flex-wrap items-center mb-6 text-xl text-gray-900 dark:text-white">
             <li>
                 <a href="../App.js" class="mr-4 hover:underline md:mr-6 ">Inicio</a>
             </li>
@@ -30,8 +29,8 @@ const Footer = () => {
             </li>
         </ul>
     </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"/>
-    <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="" class="hover:underline">La Petite</a>. Todos los derechos reservados.
+      <hr class="my-6 border-gray-700 sm:mx-auto dark:border-gray-700 lg:my-8"/>
+    <span class="block text-sm text-black-500 sm:text-center dark:text-gray-400">© 2023 <a href="" class="hover:underline">La Petite</a>. Todos los derechos reservados.
     </span>
 </footer>
 
